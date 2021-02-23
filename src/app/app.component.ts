b79ff0a850c60d7623174b7c07918e5bda5dea5f
tootzie <c14180106@john.petra.ac.id>
2021-02-23 18:32:48 +0700

    Commit latihan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  a = 0;
+  
+  b = 0;
+ 
+  item = [
+    {nama: "Jeruk", harga: 15000, satuan: "kg"},
+    {nama: "Apel", harga: 5000, satuan: "kg"}
+  ]
+
+  tampil : boolean = false;
+
+  tulisanOPEN : String;
+  OPEN() {
+    this.tulisanOPEN = "Toko dibuka";
+    alert("Dibuka");
+  }
+
+  values : "";
+  onKey(event: any) {
+    this.values += event.target.value + "|";
+  }
 }
